29516f8c3e0603bd8193e05eb2712391b9dbb434
Rick Ansay <rick.ansay@gmail.com>
2021-11-30 20:24:23 -0700

    Started JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,23 @@
-// Weather API
-// using the weather api site, create a search function utilizing the provided weather api
+// Weather JS Logic
+
+const cityFormEl = $('target');
+const cityInputEl = $('#city-input');
+const citySubmitEl = $('#city-submit');
+
+// API Key
+const APIKEY = '459011ac5cf9fbfc44816294a4c3b7aa';
+
+let queryURL = ``;
+let currentCityLat = '';
+let currentCityLong = '';
+
+cityFormEl.submit((event) => {
+    event.preventDefault();
+    
+})
+
+const pullWeatherData = () => {
+    fetch()
+}
+
+//let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`;
