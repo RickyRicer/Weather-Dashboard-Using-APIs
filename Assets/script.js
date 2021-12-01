460b464379ea4f82698bfea1cc8fcf7043af3172
Rick Ansay <rick.ansay@gmail.com>
2021-11-30 20:52:01 -0700

    More JS and HTML updates, added API Key, started logic for JS

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,7 @@
 const cityFormEl = $('target');
 const cityInputEl = $('#city-input');
 const citySubmitEl = $('#city-submit');
+const searchHistoryEl = $('#search-history');
 
 // API Key
 const APIKEY = '459011ac5cf9fbfc44816294a4c3b7aa';
@@ -10,14 +11,29 @@ const APIKEY = '459011ac5cf9fbfc44816294a4c3b7aa';
 let queryURL = ``;
 let currentCityLat = '';
 let currentCityLong = '';
+let currentCityName = '';
+
+let searchHistory = JSON.parse(localStorage.getItem('searchHistory') || "[]");
+
+function storeSearchHistory(userSearchQuery) {
+
+    searchHistory.push(userSearchQuery);
+
+    // Search
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+}
 
 cityFormEl.submit((event) => {
     event.preventDefault();
-    
-})
+    let userCity = $(cityInputEl).val();
+    queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`;
+    pullCityWeatherData();    
+});
 
-const pullWeatherData = () => {
-    fetch()
+const pullCityWeatherData = () => {
+
+    fetch(queryURL).then(response => 
+        response.json()then)
 }
 
 //let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`;
