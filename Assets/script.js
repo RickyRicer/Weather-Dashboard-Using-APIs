5d27a6596cce1bffafa4719f443b1b8eceac7f4d
Rick Ansay <rick.ansay@gmail.com>
2021-11-30 21:08:23 -0700

    More JS

--- Assets/script.js
+++ Assets/script.js
@@ -9,11 +9,23 @@ const searchHistoryEl = $('#search-history');
 const APIKEY = '459011ac5cf9fbfc44816294a4c3b7aa';
 
 let queryURL = ``;
+let currentCity = 
+{lat: 0, 
+    lon: 0, 
+    name: 'EnglandIsMyCity', 
+    date: '0000000000',
+    icon: "aaa",
+    temp: 0,
+    wind: 0,
+    humidity: 0,
+    uvi: 0,
+}
 let currentCityLat = '';
 let currentCityLong = '';
 let currentCityName = '';
 
 let searchHistory = JSON.parse(localStorage.getItem('searchHistory') || "[]");
+// Add search history below search button
 
 function storeSearchHistory(userSearchQuery) {
 
